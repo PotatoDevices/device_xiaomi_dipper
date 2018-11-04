0b5b4a579af7f7a66dad7b54a81dc765c0861030
argraur <argraur@gmail.com>
2018-11-04 17:26:19 +0000

    dipper: overlay: disable multi color battery settings
    Change-Id: I47b00739158682e3d57e0fa802b9ea4caa48e377
    Signed-off-by: argraur <argraur@gmail.com>

--- device.mk
+++ device.mk
@@ -12,7 +12,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosip
 
 # Properties
 -include $(LOCAL_PATH)/device-props.mk
