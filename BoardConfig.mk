dfa7fc03c37fb987dd69054aad7e7a08a39fe187
Bruno Martins <bgcngm@gmail.com>
2019-01-11 23:04:15 +0000

    dipper: Add device-specific fstab and flags
     * This got decommonized in order to support perseus,
       which is a system-as-root device.
    
    Change-Id: I0eba72337e7f190d86c37aa6aff893fdac61975e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -25,6 +25,12 @@ TARGET_USES_NQ_NFC := true
 
 # Face Unlock
 TARGET_FACE_UNLOCK_CAMERA_ID := 5
+
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/dipper/BoardConfigVendor.mk
