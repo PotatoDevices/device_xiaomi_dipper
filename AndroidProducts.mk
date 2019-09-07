dde4a51dbe87a868232b184b52b28d86af1fe5ba
argraur <argraur@gmail.com>
2019-09-07 07:54:46 +0000

    [P3R] dipper: move to vendor/google/product/dipper, introduce non-AOSP dipper device
    Signed-off-by: argraur <argraur@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The Pixel3ROM Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,9 +12,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_dipper.mk
+PRODUCT_MAKEFILES += \
+    vendor/google/product/dipper/aosp_dipper.mk \
+    vendor/google/product/dipper/dipper.mk
 
-COMMON_LUNCH_CHOICES := \
-    aosp_dipper-userdebug
+COMMON_LUNCH_CHOICES += \
+    aosp_dipper-userdebug \
+    dipper-userdebug \
+    dipper-user
